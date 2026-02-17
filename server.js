dec9dd23c61af7cc94e2b6bb60fd1a819c025a06
kerem <keremacar3754is@gmail.com>
2026-02-17 11:23:36 +0300

    frontend url sabitlendi

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // ===== FRONTEND URL =====
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const FRONTEND_URL = "https://gorev-sistemii.onrender.com";
 
 // ===== AUTH MIDDLEWARE =====
 function auth(req, res, next) {
