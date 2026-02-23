0a767db67678aa5f50d7bf020121d021a1284fc6
kerem <keremacar3754is@gmail.com>
2026-02-23 16:31:55 +0300

    push

--- public/script.js
+++ public/script.js
@@ -1,8 +1,5 @@
 // Backend URL'i tüm cihazlarda kullanabilmek için localStorage'den al
-const API_URL =
-  window.location.hostname.includes("onrender.com")
-    ? window.location.origin
-    : "http://localhost:3000";
+const API_URL = localStorage.getItem("API_URL") || "http://localhost:3000";
 
 // -------------------- SEND CODE --------------------
 function sendCode() {
