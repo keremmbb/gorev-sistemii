27144f1ec0ccbb21863b2eb253fb9bcd84effc37
kerem <keremacar3754is@gmail.com>
2026-02-23 11:25:28 +0300

    mail

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Backend URL'i tüm cihazlarda kullanabilmek için localStorage'den al
-const API_URL = window.location.origin;
+const API_URL = localStorage.getItem("API_URL") || "http://localhost:3000";
 
 // -------------------- SEND CODE --------------------
 function sendCode() {
