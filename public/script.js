2dd4fb6f1702ec41adbfa89c1c20c7952c5a45ad
kerem <keremacar3754is@gmail.com>
2026-02-23 11:11:01 +0300

    API_URL düzeltildi

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Backend URL'i tüm cihazlarda kullanabilmek için localStorage'den al
-const API_URL = localStorage.getItem("API_URL") || "http://localhost:3000";
+const API_URL = "https://gorev-sistemii.onrender.com";
 
 // -------------------- SEND CODE --------------------
 function sendCode() {
