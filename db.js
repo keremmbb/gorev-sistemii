914775c19131f3317bb82b995256900b1948c334
kerem <keremacar3754is@gmail.com>
2026-03-04 16:04:14 +0300

    push

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// Render'da DATABASE_URL kullanılır, yerelde ise senin .env dosmandaki tekil bilgiler.
+const isProduction = process.env.DATABASE_URL;
+
 const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : undefined,
+  user: isProduction ? undefined : process.env.DB_USER,
+  host: isProduction ? undefined : process.env.DB_HOST,
+  database: isProduction ? undefined : process.env.DB_NAME,
+  password: isProduction ? undefined : process.env.DB_PASSWORD,
+  port: isProduction ? undefined : process.env.DB_PORT,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 db.on("connect", () => {
@@ -15,6 +20,6 @@ db.on("connect", () => {
 
 db.on("error", (err) => {
   console.error("DB HATASI ❌", err);
-}); 
+});
 
 module.exports = db;
